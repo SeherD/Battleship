fdfb92727433f0b9171519bac1fd5c675b71ca53
tian-xia2 <56200169+tian-xia2@users.noreply.github.com>
2020-02-25 23:22:09 -0700

    Update Utilities.java

--- Control/Utilities.java
+++ Control/Utilities.java
@@ -2,24 +2,23 @@ package Control;
 
 import Game.Board;
 
-//import Player.Turn;
-
-//import Ships.*;
-
+// This class provides intro and end messages to the console screen.
 public class Utilities {
 	
 	private Board board = new Board();
 	//private Turn turn = new Turn();
 	
+	// Intro message is created to greet the user.
 	public void intro() {
-		System.out.println("\t\t\t*****************************************");
-	    System.out.println("\t\t\t**     WELCOME TO BATTLESHIP GAME!!    **");
-	    System.out.println("\t\t\t**  (Presented by Tutorial 2 Group 6)  **");
-	    System.out.println("\t\t\t*****************************************\n");
-	    System.out.println("\t\t\tAt the moment, there is no ship in the see.\n");
-	    board.printBoard();
+	    	System.out.println("\t\t\t*****************************************");
+	   	System.out.println("\t\t\t**     WELCOME TO BATTLESHIP GAME!!    **");
+	    	System.out.println("\t\t\t**  (Presented by Tutorial 2 Group 6)  **");
+	    	System.out.println("\t\t\t*****************************************\n");
+	    	System.out.println("\t\t\tAt the moment, there is no ship in the see.\n");
+	    	board.printBoard();
 	}
 	
+	// End message is create to announce the game winner.
 	public void end(int humanShip, int computerShip) {
 		if (humanShip > computerShip) {
 			System.out.println("Human player won! All computer's ships have been sunken.");
@@ -30,8 +29,8 @@ public class Utilities {
 		}
 		
 		System.out.println("\t\t\t*******************************************");
-	    System.out.println("\t\t\t** GAME HAS ENDED. WELCOME TO PLAY AGAIN **");
-	    System.out.println("\t\t\t**   (Presented by Tutorial 2 Group 6)   **");
-	    System.out.println("\t\t\t*******************************************");
+	    	System.out.println("\t\t\t** GAME HAS ENDED. WELCOME TO PLAY AGAIN **");
+	    	System.out.println("\t\t\t**   (Presented by Tutorial 2 Group 6)   **");
+	    	System.out.println("\t\t\t*******************************************");
 	}
 }
