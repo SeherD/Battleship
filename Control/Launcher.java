1f905da4830d655462ca77d5eadbb71c540a9469
tian-xia2 <56200169+tian-xia2@users.noreply.github.com>
2020-02-26 17:28:21 -0700

    Update Launcher.java

--- Control/Launcher.java
+++ Control/Launcher.java
@@ -4,6 +4,11 @@ import Player.Turn;
 
 import Control.Utilities;
 
+/* 
+This class controls the general flow of the game as the following:
+Intro message -> user places the ships -> computer places the ships -> Shoot at each others' ships
+-> switch turns until one side's ships are all sunken -> End message.
+*/
 public class Launcher {
 	
 	public static void main(String args[]) 
