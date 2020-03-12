cc9b7661fd2f47f71291516108f04bc02383e8df
sluong53 <60753246+sluong53@users.noreply.github.com>
2020-03-12 01:10:33 -0600

    Add files via upload

---
+++ Battleship Demo2/Ships/Destroyer.java
@@ -0,0 +1,26 @@
+package Ships;
+
+import Game.Tile;
+
+public class Destroyer extends Ship
+{
+	private final String SHIP_NAME = "Destroyer";
+	private final int SHIP_SIZE = 3;
+	private final char ID = 'D';
+	
+	public Destroyer() 
+	{
+    	super();
+    	shipName = SHIP_NAME;
+    	shipSize = SHIP_SIZE;
+    	id = ID;
+	}
+	
+	public Destroyer( Tile a, Tile b,String player)
+	{	
+		super(a, b, player);
+		shipName = SHIP_NAME;
+		this.shipSize = SHIP_SIZE;
+		this.id = ID;	
+	}
+}
