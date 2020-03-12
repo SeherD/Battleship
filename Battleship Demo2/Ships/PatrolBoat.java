e7db5e6d6babbf11b8241ffabfa565370eaa79a2
sluong53 <60753246+sluong53@users.noreply.github.com>
2020-03-12 01:09:45 -0600

    Add files via upload

---
+++ Battleship Demo2/Ships/PatrolBoat.java
@@ -0,0 +1,26 @@
+package Ships;
+
+import Game.*;
+
+public class PatrolBoat extends Ship 
+{
+	private final String SHIP_NAME = "PatrolBoat";
+    private final int SHIP_SIZE = 2;
+    private final char ID = 'P';
+    
+    public PatrolBoat() 
+    {
+		super();
+		shipName = SHIP_NAME;
+    	shipSize = SHIP_SIZE;
+    	id = ID;
+    }
+    
+    public PatrolBoat(Tile a, Tile b, String player)
+	{	
+		super(a, b, player);
+	    shipName = SHIP_NAME;
+	    this.shipSize = SHIP_SIZE;
+	    this.id = ID;
+	}
+}
