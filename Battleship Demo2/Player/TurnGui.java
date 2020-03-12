2b2c5aab143302218292d01d60a32c092680bff1
tian-xia2 <56200169+tian-xia2@users.noreply.github.com>
2020-03-11 22:36:58 -0600

    Update TurnGui.java

--- Battleship Demo2/Player/TurnGui.java
+++ Battleship Demo2/Player/TurnGui.java
@@ -2,17 +2,21 @@ package Player;
 
 import Game.Board;
 import javafx.scene.control.Button;
+
+// This class uses the logic code's 2D array elements' values for button clicks
 public class TurnGui {
 	private Board b=new Board();
 	
 	public void humanTurn(int x,int y, Button button) {
 	
+		// When the target tile is empty.
 		if (b.getBoard2()[x][y] == 0) 
 		{	button.setStyle("-fx-background-color: blue;");
 			System.out.println("You missed!");
 			//System.out.println(b.getBoard2()[x][y]);
 			b.setBoard2(x, y, 4);
 		} 
+		// When the target tile is a ship Tile.
 		else if (b.getBoard2()[x][y] == 2)
 		{	button.setStyle("-fx-background-color: red;");
 			System.out.println("You hit the computer's ship!");
@@ -20,6 +24,7 @@ public class TurnGui {
 			b.setBoard2(x, y, 3);
 			//computerShipsRemaining -= 1;
 		}
+		// When the tile has been shot at.
 		else if (b.getBoard2()[x][y] == 3||b.getBoard2()[x][y] == 4)
 		{	
 			System.out.println("You have already shot here");
