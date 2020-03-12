19b5e0fe7b2d8e38183dd0d54542c71451ff97cc
sluong53 <60753246+sluong53@users.noreply.github.com>
2020-03-12 01:11:18 -0600

    Add files via upload

---
+++ Battleship Demo2/Ships/Cruiser.java
@@ -0,0 +1,26 @@
+package Ships;
+
+import Game.*;
+
+public class Cruiser extends Ship 
+{
+	private final String SHIP_NAME = "Battleship";
+    private final int SHIP_SIZE = 4;
+    private final char ID = 'B';
+    
+    public Cruiser() 
+    {
+    	super();
+    	shipName = SHIP_NAME;
+    	shipSize = SHIP_SIZE;
+    	id = ID;
+    }
+    
+    public Cruiser(Tile a, Tile b, String player)
+	{	
+		super(a, b, player);
+	    shipName = SHIP_NAME;
+	    this.shipSize = SHIP_SIZE;
+	    this.id = ID;
+	}
+}
