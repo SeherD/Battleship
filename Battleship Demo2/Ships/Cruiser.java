06d364bce3a80569cbe93c1cfe07821cc50dc133
sluong53 <60753246+sluong53@users.noreply.github.com>
2020-03-13 00:57:24 -0600

    Update Cruiser.java

--- Battleship Demo2/Ships/Cruiser.java
+++ Battleship Demo2/Ships/Cruiser.java
@@ -2,9 +2,13 @@ package Ships;
 
 import Game.*;
 
+/* 
+ * Cruiser is one of the five ships on each side.
+ * This class is a sub class of Ship.
+*/ 
 public class Cruiser extends Ship 
 {
-	private final String SHIP_NAME = "Battleship";
+    private final String SHIP_NAME = "Battleship";
     private final int SHIP_SIZE = 4;
     private final char ID = 'B';
     
