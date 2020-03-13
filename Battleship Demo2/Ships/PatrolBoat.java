65ab64827f2cb1a5322f061db1946928a3ab8430
sluong53 <60753246+sluong53@users.noreply.github.com>
2020-03-13 01:12:22 -0600

    Update PatrolBoat.java

--- Battleship Demo2/Ships/PatrolBoat.java
+++ Battleship Demo2/Ships/PatrolBoat.java
@@ -2,9 +2,13 @@ package Ships;
 
 import Game.*;
 
+/* 
+ * Patrol boat is one of the five ships on each side.
+ * This class is a sub class of Ship.
+*/ 
 public class PatrolBoat extends Ship 
 {
-	private final String SHIP_NAME = "PatrolBoat";
+    private final String SHIP_NAME = "PatrolBoat";
     private final int SHIP_SIZE = 2;
     private final char ID = 'P';
     
