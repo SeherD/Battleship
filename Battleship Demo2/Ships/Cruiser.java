e69f34335cbf5bb2dead2365d2848f4915d08b31
sluong53 <60753246+sluong53@users.noreply.github.com>
2020-03-13 01:00:16 -0600

    Update Cruiser.java

--- Battleship Demo2/Ships/Cruiser.java
+++ Battleship Demo2/Ships/Cruiser.java
@@ -21,9 +21,13 @@ public class Cruiser extends Ship
     	id = ID;
     }
     
+    /* 
+     * Constructor with parameters: 
+     * 1: Start tile, 2: End tile, 3: player.
+    */ 
     public Cruiser(Tile a, Tile b, String player)
 	{	
-		super(a, b, player);
+	    super(a, b, player);
 	    shipName = SHIP_NAME;
 	    this.shipSize = SHIP_SIZE;
 	    this.id = ID;
