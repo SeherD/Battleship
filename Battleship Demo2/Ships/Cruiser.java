e292afdbca2f6c28a88b0688d016cced49c36d3c
sluong53 <60753246+sluong53@users.noreply.github.com>
2020-03-13 23:38:12 -0600

    Add files via upload

--- Battleship Demo2/Ships/Cruiser.java
+++ Battleship Demo2/Ships/Cruiser.java
@@ -2,34 +2,22 @@ package Ships;
 
 import Game.*;
 
-/* 
- * Cruiser is one of the five ships on each side.
- * This class is a sub class of Ship.
-*/ 
-public class Cruiser extends Ship 
-{
-    private final String SHIP_NAME = "Battleship";
-    private final int SHIP_SIZE = 4;
-    private final char ID = 'B';
-    
-    //Constructor with no parameter.
-    public Cruiser() 
-    {
-    	super();
-    	shipName = SHIP_NAME;
-    	shipSize = SHIP_SIZE;
-    	id = ID;
-    }
-    
-    /* 
-     * Constructor with parameters: 
-     * 1: Start tile, 2: End tile, 3: player.
-    */ 
-    public Cruiser(Tile a, Tile b, String player)
-	{	
-	    super(a, b, player);
-	    shipName = SHIP_NAME;
-	    this.shipSize = SHIP_SIZE;
-	    this.id = ID;
+public class Cruiser extends Ship {
+	private final String SHIP_NAME = "Battleship";
+	private final int SHIP_SIZE = 4;
+	private final char ID = 'B';
+	
+	public Cruiser() {
+		super();
+		shipName = SHIP_NAME;
+		shipSize = SHIP_SIZE;
+		id = ID;
+	}
+	
+	public Cruiser(Tile a, Tile b, String player) {
+		super(a, b, player);
+		shipName = SHIP_NAME;
+		this.shipSize = SHIP_SIZE;
+		this.id = ID;
 	}
 }
