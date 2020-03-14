e4a785ac321ed45426af868228a6098ac71b0227
TotoroCaty <60753066+TotoroCaty@users.noreply.github.com>
2020-03-14 16:46:34 -0600

    Update Main.java

--- Main Menu/Main.java
+++ Main Menu/Main.java
@@ -8,9 +8,12 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
+/**
+ * This class, Main extends from Application and implements Event Handler
+ * @author T2G6: Seher Dawar, Tian Xia, Jessica Tran and Spencer Luong.
+ */
+
 public class Main extends Application implements EventHandler<ActionEvent>{
-	
-	Button button;
 	
 	public static void main(String[] args) {
 		launch(args);
@@ -18,40 +21,57 @@ public class Main extends Application implements EventHandler<ActionEvent>{
 	
 	public void start(Stage stage) {
 		
-		stage.setTitle("Battleship");
+		stage.setTitle("BATTLESHIP");
 		
-		// Code to add background image
-		Image background = new Image("file:Battleship MainScreen.png");
+		// Set the background for the scene
+		Image background = new Image("file:1200x700 MainMenu.png");
 		ImageView imBackground = new ImageView(background);
 		
         Button startButton = new Button();
-        startButton.setTranslateX(115);
-        startButton.setTranslateY(490);
+        startButton.setTranslateX(100);
+        startButton.setTranslateY(485);
         startButton.setGraphic(new ImageView("file:StartButton.png"));
         startButton.setStyle("  -fx-border-style: none; -fx-border-width: 2px; -fx-border-insets: 0; -fx-font-size:4px");
         startButton.setStyle("-fx-background-color: transparent;");
        
-        Button exitButton = new Button();
-        exitButton.setTranslateX(925);
-        exitButton.setTranslateY(3);
-        exitButton.setGraphic(new ImageView("file:ExitButton.png"));
-        exitButton.setStyle("  -fx-border-style: none; -fx-border-width: 2px; -fx-border-insets: 0; -fx-font-size:4px");
-        exitButton.setStyle("-fx-background-color: transparent;");
+        Button musicButton = new Button();
+        musicButton.setTranslateX(1127);
+        musicButton.setTranslateY(3);
+        musicButton.setGraphic(new ImageView("file:MusicON.png"));
+        musicButton.setStyle("  -fx-border-style: none; -fx-border-width: 2px; -fx-border-insets: 0; -fx-font-size:4px");
+        musicButton.setStyle("-fx-background-color: transparent;");
+        	
+        Button helpButton = new Button();
+        helpButton.setTranslateX(435);
+        helpButton.setTranslateY(557);
+        helpButton.setGraphic(new ImageView("file:HelpButton.png"));
+        helpButton.setStyle("  -fx-border-style: none; -fx-border-width: 2px; -fx-border-insets: 0; -fx-font-size:4px");
+        helpButton.setStyle("-fx-background-color: transparent;");
         
+        // This button should be a combo box, it is button right now to show it's placement of scene
+        Button modeButton = new Button();
+        modeButton.setTranslateX(435);
+        modeButton.setTranslateY(485);
+        modeButton.setGraphic(new ImageView("file:ModeButton.png"));
+        modeButton.setStyle("  -fx-border-style: none; -fx-border-width: 2px; -fx-border-insets: 0; -fx-font-size:4px");
+        modeButton.setStyle("-fx-background-color: transparent;");
+         
 		Group root = new Group();
 		root.getChildren().addAll(imBackground);
 		root.getChildren().addAll(startButton);
-		root.getChildren().addAll(exitButton);
+		root.getChildren().addAll(musicButton);
+		root.getChildren().addAll(helpButton);
+		root.getChildren().addAll(modeButton);
 		
-		Scene scene = new Scene(root, 1000, 700);
+		Scene scene = new Scene(root, 1200, 700);
 		stage.setScene(scene);
-		//false if you don't want to resize your form
+		// Set to false if you don't want resize your scene
 		stage.setResizable(false);
 		stage.show();
 		
-		
 		startButton.setOnAction(new EventHandler<ActionEvent>() {
 		@Override
+		// This button will take us to the game play scene
         public void handle(ActionEvent event) {
 			if (event.getSource() == startButton) {
 				startButton.setGraphic(new ImageView("file:StartButtonClicked.png"));
@@ -61,14 +81,39 @@ public class Main extends Application implements EventHandler<ActionEvent>{
 				}
 			}
 		});
-		exitButton.setOnAction(new EventHandler<ActionEvent>() {
+		// Not sure how to make this button change from Music ON and Music OFF yet
+		musicButton.setOnAction(new EventHandler<ActionEvent>() {
 			@Override
 	        public void handle(ActionEvent event) {
-				if (event.getSource() == exitButton) {
-					stage.close();
+				if (event.getSource() == musicButton) {
+					musicButton.setGraphic(new ImageView("file:MusicOFF.png"));
+					musicButton.setStyle("  -fx-border-style: none; -fx-border-width: 2px; -fx-border-insets: 0; -fx-font-size:4px");
+					musicButton.setStyle("-fx-background-color: transparent;");
+					System.out.println("Music OFF");
 				}
 			}
-		}); 
+		});
+		// This button will take us to a new scene for more help options
+		helpButton.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+	        public void handle(ActionEvent event) {
+				if (event.getSource() == helpButton) {
+					helpButton.setGraphic(new ImageView("file:HelpButtonClicked.png"));
+					helpButton.setStyle("  -fx-border-style: none; -fx-border-width: 2px; -fx-border-insets: 0; -fx-font-size:4px");
+					helpButton.setStyle("-fx-background-color: transparent;");
+					System.out.println("Help Button Clicked");
+				}
+			}
+		});
+		// Mode button should be a combo box
+		modeButton.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+	        public void handle(ActionEvent event) {
+				if (event.getSource() == modeButton) {
+					System.out.println("Mode Button Clicked");
+				}
+			}
+		});	
 	}
 
 	@Override
