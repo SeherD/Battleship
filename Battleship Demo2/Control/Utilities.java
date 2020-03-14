146023425be02f6c61bce0feb8f960294ac1e247
tian-xia2 <56200169+tian-xia2@users.noreply.github.com>
2020-03-13 23:21:27 -0600

    Add files via upload

--- Battleship Demo2/Control/Utilities.java
+++ Battleship Demo2/Control/Utilities.java
@@ -1,3 +1,4 @@
+
 package Control;
 
 import Game.Board;
@@ -9,46 +10,54 @@ import Player.Turn;
 
 //import Ships.*;
 
+/**
+ * This class provides messages to the user, including:
+ * 		inro and end messgaes in the console and win information box on GUI.
+ * @author T2G6: Seher Dawar, Tian Xia, Jessica Tran and Spencer Luong.
+ * @version 2.21: GUI March 2020
+ */
 public class Utilities {
-	
+
 	private Board board = new Board();
-	//private Turn turn = new Turn();
-	
-	// Intro messages in console.
+
+	/**
+	 * This method prints into messages to the console.
+	 */
 	public void intro() {
 		System.out.println("\t\t\t*****************************************");
-	    System.out.println("\t\t\t**     WELCOME TO BATTLESHIP GAME!!    **");
-	    System.out.println("\t\t\t**  (Presented by Tutorial 2 Group 6)  **");
-	    System.out.println("\t\t\t*****************************************\n");
-	    System.out.println("\t\t\tAt the moment, there is no ship in the sea.\n");
-	    board.printBoard();
+		System.out.println("\t\t\t**     WELCOME TO BATTLESHIP GAME!!    **");
+		System.out.println("\t\t\t**  (Presented by Tutorial 2 Group 6)  **");
+		System.out.println("\t\t\t*****************************************\n");
+		System.out.println("\t\t\tAt the moment, there is no ship in the sea.\n");
+		board.printBoard();
 	}
-	
-	// End messages in console.
-	public void end(int humanShip, int computerShip,GridPane grid,Turn turn) {
+	/**
+	 * This method prints the end message in the console.
+	 * @param humanShip human's remaining ship Tiles
+	 * @param computerShip computer's remaining ship Tiles
+	 * @param grid JavaFX'ss gridPane
+	 * @param turn Logic code's Turn
+	 */
+	public void end(int humanShip, int computerShip, GridPane grid, Turn turn) {
 		String endMsg = "";
-		
-		if ( computerShip==0) {
+		if (computerShip == 0) {
 			endMsg = "Human player won! All computer's ships have been sunken.";
-		}else if (humanShip==0) {
+		} else if (humanShip == 0) {
 			endMsg = "Computer player won! All human's ships have been sunken.";
-		}else if (humanShip == computerShip) {
+		} else if (humanShip == computerShip) {
 			endMsg = "Both players are winners! Both players' ships have been sunken.";
 		}
-		
 		System.out.println(endMsg);
-		
 		System.out.println("\t\t\t*******************************************");
-	    	System.out.println("\t\t\t** GAME HAS ENDED. WELCOME TO PLAY AGAIN **");
-	    	System.out.println("\t\t\t**   (Presented by Tutorial 2 Group 6)   **");
-	    	System.out.println("\t\t\t*******************************************");
-	    	turn.nullify(grid);
-		
-		// Info box pop up is displayed once the game ends.
-    		Alert badGrade = new Alert(AlertType.INFORMATION);
+		System.out.println("\t\t\t** GAME HAS ENDED. WELCOME TO PLAY AGAIN **");
+		System.out.println("\t\t\t**   (Presented by Tutorial 2 Group 6)   **");
+		System.out.println("\t\t\t*******************************************");
+		turn.nullify(grid);
+
+		// Info box pop up is displayed on GUI once the game ends.
+		Alert badGrade = new Alert(AlertType.INFORMATION);
 		badGrade.setTitle("Game Over");
 		badGrade.setHeaderText(endMsg);
 		badGrade.showAndWait();
-	   
 	}
 }
