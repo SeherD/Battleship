99f260ab4b298da8e07547b114f435badf44ca2d
sluong53 <60753246+sluong53@users.noreply.github.com>
2020-03-13 23:39:43 -0600

    Add files via upload

--- Battleship Demo2/Ships/PatrolBoat.java
+++ Battleship Demo2/Ships/PatrolBoat.java
@@ -2,34 +2,22 @@ package Ships;
 
 import Game.*;
 
-/* 
- * Patrol boat is one of the five ships on each side.
- * This class is a sub class of Ship.
-*/ 
-public class PatrolBoat extends Ship 
-{
-    private final String SHIP_NAME = "PatrolBoat";
-    private final int SHIP_SIZE = 2;
-    private final char ID = 'P';
-    
-    //Constructor with no parameter.
-    public PatrolBoat() 
-    {
-	super();
-	shipName = SHIP_NAME;
-    	shipSize = SHIP_SIZE;
-    	id = ID;
-    }
-    
-    /* 
-     * Constructor with parameters: 
-     * 1: Start tile, 2: End tile, 3: player.
-    */ 
-    public PatrolBoat(Tile a, Tile b, String player)
-    	}
-	    super(a, b, player);
-	    shipName = SHIP_NAME;
-	    this.shipSize = SHIP_SIZE;
-	    this.id = ID;
+public class PatrolBoat extends Ship {
+	private final String SHIP_NAME = "PatrolBoat";
+	private final int SHIP_SIZE = 2;
+	private final char ID = 'P';
+	
+	public PatrolBoat() {
+		super();
+		shipName = SHIP_NAME;
+		shipSize = SHIP_SIZE;
+		id = ID;
+	}
+	
+	public PatrolBoat(Tile a, Tile b, String player) {
+		super(a, b, player);
+		shipName = SHIP_NAME;
+		this.shipSize = SHIP_SIZE;
+		this.id = ID;
 	}
 }
